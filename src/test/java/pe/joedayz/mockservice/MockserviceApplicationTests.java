6e716480f5c6d063e0aadd32f870b96c0088ff2e
Jose Amadeo Diaz Diaz <jose.diaz@joedayz.pe>
2025-01-08 22:50:52 -0500

    primera version

---
+++ src/test/java/pe/joedayz/mockservice/MockserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.joedayz.mockservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
